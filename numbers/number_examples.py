325209781a55c16939863ffeb6099aa5ec42d321
jared-ong <jong@mdsol.com>
2020-02-28 04:14:47 +0000

    Create number_examples.py
    Create number_examples.py

---
+++ numbers/number_examples.py
@@ -0,0 +1,71 @@
+# BOOLEANS - True or False
+bool(True)
+bool(1)
+bool(-45)
+bool(False)
+bool(0)
+bool(0.0)
+
+
+# INTEGERS (42 and 10000000)
+5
+0
+# This will throw an error
+05
+
+123
++123
+-123
+
+# This will throw an error
+1,000,000
+# The underscore is a valid separator
+million = 1_000_000
+million
+
+
+# Integer Operations
+# Addition
+x = 5 + 8
+x
+# Subtraction
+x = 90 - 10
+x
+# Multiplication
+x = 4 * 7
+x
+# Floating point division
+x = 7/2
+x
+# Integer truncating division
+x = 7//2
+x
+# Modulus
+x = 7 % 3
+x
+# Exponentiation
+x = 3 ** 4
+x
+
+# Addition and Subtraction
+5 + 9
+100 - 7
+4 - 10
+5 + 9 + 3
+4 + 3 - 2 - 1 + 6
+# Not required to have a space between each number
+5+9          +     3
+
+# Multiplication
+6 * 7
+6 * 7 * 2 * 3
+# Division
+9 / 5
+9 // 5
+# Divide by 0 throws error as expected
+5 / 0
+
+
+
+
+# floats (numbers with decimal points 12.3333333 or 1.0e8)
