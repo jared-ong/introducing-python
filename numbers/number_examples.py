c1cf04fbda88738905c3c1241ba4769f4147eef6
jared-ong <jong@mdsol.com>
2020-02-28 18:04:28 +0000

    Updates to numbers
    Updates to numbers

--- numbers/number_examples.py
+++ numbers/number_examples.py
@@ -1,71 +1,194 @@
 # BOOLEANS - True or False
-bool(True)
-bool(1)
-bool(-45)
-bool(False)
-bool(0)
-bool(0.0)
+x = bool(True)
+print(x)
+x = bool(1)
+print(x)
+x = bool(-45)
+print(x)
+x = bool(False)
+print(x)
+x = bool(0)
+print(x)
+x = bool(0.0)
+print(x)
 
 
 # INTEGERS (42 and 10000000)
-5
-0
+x = 5
+print(x)
+x = 0
+print(x)
 # This will throw an error
 05
+x = 05
 
-123
-+123
--123
+x = +123
+print(x)
+x = 123
+print(x)
+x = -123
+print(x)
 
 # This will throw an error
 1,000,000
+x = 1,000,000
+
 # The underscore is a valid separator
-million = 1_000_000
-million
+x = 1_000_000
+type(x)
+print(x)
+x = 0000
+print(x)
+
 
 
 # Integer Operations
 # Addition
 x = 5 + 8
-x
+print(x)
 # Subtraction
 x = 90 - 10
-x
+print(x)
 # Multiplication
 x = 4 * 7
-x
+print(x)
 # Floating point division
 x = 7/2
-x
+print(x)
 # Integer truncating division
 x = 7//2
-x
+print(x)
 # Modulus
 x = 7 % 3
-x
+print(x)
 # Exponentiation
 x = 3 ** 4
-x
+print(x)
 
 # Addition and Subtraction
-5 + 9
-100 - 7
-4 - 10
-5 + 9 + 3
-4 + 3 - 2 - 1 + 6
+x = 5 + 9
+print(x)
+x = 100 - 7
+print(x)
+x = 4 - 10
+print(x)
+x = 5 + 9 + 3
+print(x)
+x = 4 + 3 - 2 - 1 + 6
+print(x)
 # Not required to have a space between each number
-5+9          +     3
+x = 5+9          +     3
+print(x)
 
 # Multiplication
-6 * 7
-6 * 7 * 2 * 3
+x = 6 * 7
+print(x)
+x = 6 * 7 * 2 * 3
+print(x)
 # Division
-9 / 5
-9 // 5
+x = 9 / 5
+print(x)
+x = 9 // 5
+print(x)
 # Divide by 0 throws error as expected
-5 / 0
+x = 5 / 0
 
 
 
 
 # floats (numbers with decimal points 12.3333333 or 1.0e8)
+x = 5.0
+print(x)
+x = 05.0
+print(x)
+x = 5.
+print(x)
+x = 5e0
+print(x)
+x = 5e1
+print(x)
+
+
+
+# Python precedence
+x = 2 + 3 * 4
+print(x)
+x = 2 + (3 * 4)
+print(x)
+x = (2 + 3) * 4
+print(x)
+
+
+# Bases
+# Integers are assumed to be base 10 unless you use a prefix to specify another
+x = 10
+print(x)
+# Binary (Base 2 - uses 0 & 1)
+# One (decimal) two and 0 ones
+x = 0b10
+print(x)
+x = 0B10
+print (x)
+# Octal (Base 8 - uses 0 - 7)
+# One (decimal) eight and 7 ones
+x = 0o17
+print(x)
+# Hex (Base 16 - uses 0 - 9 and A - F )
+# 4 (decimal) sixteen and 1 ones
+x = 0x41
+print(x)
+
+# bin(), oct(), hex(), chr(), ord('A')
+x = 65
+y = bin(x)
+print(y)
+y = oct(x)
+print(y)
+y = hex(x)
+print(y)
+# chr function
+x = chr(65)
+print(x)
+# ord goes the other way
+x = ord('A')
+print(x)
+
+
+
+# Type conversions
+# int() - in Python 2 int was -2,147,483,648 to 2,147,483,648 but in Python 3 an int can be any size including a googol (one followed by 100 zeroes, named in 1920 by a 9 year old boy.
+# Googolplex - 1 plus a thousand zeroes originally suggested as the name for Google, but they settled on googol but didn't check it's spelling before registering google.com domain
+x = int(True)
+print(x)
+x = int(False)
+print(x)
+x = int('99')
+print(x)
+
+
+# bool()
+x = bool(1)
+print(x)
+x = bool(0)
+print(x)
+x = bool(55)
+print(x)
+
+
+# float
+x = float(True)
+print(x)
+x = float(False)
+print(x)
+x = float(98)
+print(x)
+x = float('1.0e4')
+print(x)
+
+# Python also promotes booleans to integers to floats
+x = False + 0
+print(x)
+x = False + 0.0
+print(x)
+x = True + 1
+print(x)
